3a75135f29d0aab5a5f05d6fd5e0d2d7ce6e477f
Twindle-001 <paul.piccolini@gmail.com>
2026-03-21 01:25:39 +0100

    fix: CSS workaround for Sanity PaneLayout deadlock

--- src/app/(studio)/studio/[[...tool]]/page.tsx
+++ src/app/(studio)/studio/[[...tool]]/page.tsx
@@ -4,5 +4,17 @@ import { NextStudio } from "next-sanity/studio";
 import config from "../../../../../sanity.config";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return (
+    <>
+      {/* Fix: Sanity PaneLayout deadlock - hidden panes block ResizeObserver,
+         keeping layout collapsed. This CSS ensures panes remain measurable. */}
+      <style>{`
+        [data-ui="PaneLayout"] > [hidden] {
+          display: block !important;
+          visibility: visible !important;
+        }
+      `}</style>
+      <NextStudio config={config} />
+    </>
+  );
 }
